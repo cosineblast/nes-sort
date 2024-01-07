ad59fdc9f4fd42697df5013d5498fb5d7bd00427
cosineblast <55855728+cosineblast@users.noreply.github.com>
2024-01-07 15:56:29 -0300

    Remove trailing space from coroutine.s

--- src/coroutine.s
+++ src/coroutine.s
@@ -1,36 +1,36 @@
 
-;; coroutine.s 
+;; coroutine.s
 ;; Implementation for the sorting coroutine.
 
 ;; This program implements a coroutine mechanism for the sorting algorithms.
-;; When the sorting algorithm is to be started, one calls the  
+;; When the sorting algorithm is to be started, one calls the
 ;; coroutine_resume subroutine for the first time.
 
 ;; When coroutine_resume is executed, it copies the execution context
 ;; (stack, S register)
 ;; to a spare memory location.
 ;; After that is done, if coroutine_resume is being
-;; executed for the first time, it will 
-;; reset the execution context to a default one, and  
+;; executed for the first time, it will
+;; reset the execution context to a default one, and
 ;; jump to coroutine_start_location, thus starting the coroutine execution.
 
 ;; The coroutine code should eventually call the coroutine_yield function.
 ;; Whenever that happens, the execution context (stack, locals, S register)
-;; is copied to a spare location, and the execution context saved 
-;; earlier by coroutine_resume is restored. coroutine_yield 
+;; is copied to a spare location, and the execution context saved
+;; earlier by coroutine_resume is restored. coroutine_yield
 ;; will then jump to the return location of the earlier coroutine_resume call.
 
 ;; After the 'return' of coroutine_resume (performed by coroutine_yield),
-;; the code may decide to call coroutine_resume once again. Once that happens, 
-;; the execution will be saved. 
+;; the code may decide to call coroutine_resume once again. Once that happens,
+;; the execution will be saved.
 
-;; However, this time, instead of resetting the execution context and jumping 
-;; to coroutine_start_location, coroutine_resume will restore the execution 
+;; However, this time, instead of resetting the execution context and jumping
+;; to coroutine_start_location, coroutine_resume will restore the execution
 ;; context saved in the latest coroutine_yield call, and will jump to its return location.
 
 ;; It is worth noting that coroutine_yield saves the local registers, but coroutine_resume doesn't.
-;; This is intentional, and one of the side effects of this, is that 
-;; after coroutine_resume returns, it is possible to inspect 
+;; This is intentional, and one of the side effects of this, is that
+;; after coroutine_resume returns, it is possible to inspect
 ;; the local variables of the coroutine at yield point.
 
 .export coroutine_yield
@@ -44,11 +44,11 @@
 
 ;; helper macros
 
-;; Swaps the 6502 stack ($0100..$01FF) with the 
+;; Swaps the 6502 stack ($0100..$01FF) with the
 ;; coroutine_stack.
 ;; Clobbers: A, X, Y
 .macro swap_stacks
-; to do: don't copy entire stack, copy only the stack used 
+; to do: don't copy entire stack, copy only the stack used
 ; up to the stack registers.
 
 ldx #$00                      ; i = 0
@@ -57,13 +57,13 @@ ldx #$00                      ; i = 0
   ldy coroutine_stack_start, x;   @swap(&stack[i], &coroutine_stack[i])
   sta coroutine_stack_start, x;
   tya                         ;   // sty can't do absolute, x access
-  sta stack_start, x          
+  sta stack_start, x
   inx                         ;   i++;
   bne @stack_copy_loop        ; } while (i != 0)
 .endmacro
 
 .macro swap_s_registers
-  tsx 
+  tsx
   ldy coroutine_s_register
   stx coroutine_s_register
   tya
@@ -73,12 +73,12 @@ ldx #$00                      ; i = 0
 
 .proc coroutine_yield
   ;; save local registers
-  ;; note: yield doesn't have to restore 
+  ;; note: yield doesn't have to restore
   ;;  the local registers of the current resume call
   ;;  so we don't have to swap the locals with coroutine_locals
   ldx #LOCAL_REGISTER_COUNT-1         ;   i = LOCAL_REGISTER_COUNT - 1;
 @local_copy_loop:                     ;   while (i >= 0) {
-  lda local0, x                     ;     
+  lda local0, x                     ;
   sta coroutine_local0, x           ;    *(coroutine_local0 + i) = *(local0 + 1);
   dex                                 ;     i -= 1;
   bpl @local_copy_loop                ;   }
@@ -86,16 +86,16 @@ ldx #$00                      ; i = 0
   ;; swap 6502 stack with coroutine_stack
   swap_stacks
 
-  ;; swap the stack pointer register 
+  ;; swap the stack pointer register
   swap_s_registers
 
   rts
 .endproc
 
 .proc coroutine_resume
-  swap_s_registers
-
   swap_stacks
+
+  swap_s_registers
 
  ;; restore local registers
 
@@ -107,12 +107,12 @@ ldx #$00                      ; i = 0
   bpl @local_copy_loop       ;   }
 
   ;; as we have restored all relevant information (S, locals, stack)
-  ;; the stack pointer points to the return address of the jsr call to yield 
-  ;; so we can rts to where yield left off. however, if this is the first 
+  ;; the stack pointer points to the return address of the jsr call to yield
+  ;; so we can rts to where yield left off. however, if this is the first
   ;; time we're running, then there's nowhere we 'left off', so we jump to our
   ;; start location instead
 
-  lda coroutine_started ; 
+  lda coroutine_started ;
   beq :+
   rts
   :
